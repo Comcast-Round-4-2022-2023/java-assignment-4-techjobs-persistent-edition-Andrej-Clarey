57d51b8db53f764573600ef692941ff40c89b202
Andrej <138933791+Andrej-Clarey@users.noreply.github.com>
2023-09-22 15:18:32 -0400

    task2 created repositories

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+//    @Id
+//    @GeneratedValue
+//    private int id;
 
+
+    @NotBlank
+    @Size(min = 5, max = 75)
+    private String location;
+
+    public Employer(){
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
